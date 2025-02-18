992ac499d55329d6810580b702f290ac368f55e8
Sameer Qureshi <sameer15557@gmail.com>
2025-02-18 15:27:14 -0500

    first commit

---
+++ classes/deliverytruck.py
@@ -0,0 +1,4 @@
+class DeliveryTruck:
+    def __init__ (self, range, active):
+        self.range = range
+        self.active = active
---
+++ classes/warehouse.py
@@ -0,0 +1,6 @@
+class Warehouse:
+    def __init__(self, capacity, radius):
+        self.capacity = capacity
+        self.radius = radius
+        
+    
