f9207ff911a6f83d8746b31754ed19b160c5f5ba
Damin <damin.joo0725@gmail.com>
2025-03-09 17:26:12 -0400

    file mix up change

--- classes/VehicleManager.py
+++ classes/VehicleManager.py
@@ -1,18 +1,25 @@
 from threading import Lock
+import time
 
 class VehicleManager:
-    def __init__(self, num_vehicles):
-        self.num_vehicles = num_vehicles
-        self.lock = Lock()
+    def __init__(self, vehicle_id):
+        self.vehicle_id = vehicle_id
+        self.is_available = True
+        self.lock = Lock()  # Mutex lock for vehicle availability
 
     def allocate_vehicle(self):
+        """Try to acquire the vehicle, blocking until available."""
         with self.lock:
-            if self.num_vehicles > 0:
-                self.num_vehicles -= 1
+            if self.is_available:
+                self.is_available = False
+                print(f"Vehicle {self.vehicle_id} allocated.")
                 return True
             else:
+                print(f"Vehicle {self.vehicle_id} is currently in use.")
                 return False
 
     def release_vehicle(self):
+        """Release the vehicle after delivery is done."""
         with self.lock:
-            self.num_vehicles += 1
+            self.is_available = True
+            print(f"Vehicle {self.vehicle_id} is now available.")
--- classes/deliverytruck.py
+++ classes/deliverytruck.py
@@ -1,31 +1,4 @@
-# class DeliveryTruck:
-#     def __init__ (self, range, active):
-#         self.range = range
-#         self.active = active
-
-from threading import Lock
-import time
-
 class DeliveryTruck:
-    def __init__(self, vehicle_id):
-        self.vehicle_id = vehicle_id
-        self.is_available = True
-        self.lock = Lock()  # Mutex lock for vehicle availability
-
-    def allocate_vehicle(self):
-        """Try to acquire the vehicle, blocking until available."""
-        with self.lock:
-            if self.is_available:
-                self.is_available = False
-                print(f"Vehicle {self.vehicle_id} allocated.")
-                return True
-            else:
-                print(f"Vehicle {self.vehicle_id} is currently in use.")
-                return False
-
-    def release_vehicle(self):
-        """Release the vehicle after delivery is done."""
-        with self.lock:
-            self.is_available = True
-            print(f"Vehicle {self.vehicle_id} is now available.")
-
+    def __init__ (self, range, active):
+        self.range = range
+        self.active = active
